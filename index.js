afa81755127c4b1144b014720d6d0e906f3e0dac
Houben,Bram B <bram.houben@student.fontys.nl>
2021-12-30 18:57:16 +0100

    backendstorenextjs

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const https = require("https");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+// });
+server.listen(4000, () => {
+  console.log("listening on *:4000");
+});
+
+app.get("/product", (req, res) => {
+  return res.json({ id: 1, desc: "prod1", price: 165 });
+});
